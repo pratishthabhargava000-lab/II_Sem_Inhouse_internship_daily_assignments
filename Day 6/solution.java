e1ac6d94587ce3c6c9c5b7110c5ab694ebfbc86d
pratishthabhargava000-lab <pratishthabhargava000@gmail.com>
2026-07-06 22:21:10 +0530

    Create solution.java

---
+++ Day 6/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) {
+        String name = "deepika";
+        int salary = 10000;
+        int bonus = 0;
+
+        System.out.printf("%-30s%-20d%d%n", name, salary, bonus);
+    }
+}
