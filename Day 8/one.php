30b02d35ff998e013246c8b944e29838caa6688a
pratishthabhargava000-lab <pratishthabhargava000@gmail.com>
2026-07-06 22:27:01 +0530

    Create one.php

---
+++ Day 8/one.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_GET["name"];
+$branch = $_GET["branch"];
+$email = $_GET["email"];
+$phonenumber = $_GET["phonenumber"];
+echo"values received:
+$name $branch $email
+$phonenumber";
+?>
