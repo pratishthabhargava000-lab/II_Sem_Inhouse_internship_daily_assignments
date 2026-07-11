ca0f93e635516e468cb57f274c3a55a0058611d2
pratishthabhargava000-lab <pratishthabhargava000@gmail.com>
2026-07-11 12:39:08 +0530

    Create footer.php

---
+++ Day 10/footer.php
@@ -0,0 +1,14 @@
+
+<footer class="bg-dark text-white text-centre py-3 mt-5">
+    <p class="mb-0" style="text-align=centre" >
+                   & copy; 
+        <?php echo date("Y"); 
+    ?>
+    My Website. All Rights Reserved.</p>
+</footer>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
