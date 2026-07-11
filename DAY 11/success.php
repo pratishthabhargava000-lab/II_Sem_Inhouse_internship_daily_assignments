58bc2f5f760f9e4b05ddfa34fe72ef5756fe3043
pratishthabhargava000-lab <pratishthabhargava000@gmail.com>
2026-07-11 13:12:30 +0530

    Create success.php

---
+++ DAY 11/success.php
@@ -0,0 +1,13 @@
+<?php
+include("header.php");
+?>
+<div class="container mt-5 text-centre">
+    <div class="alerrt alert-success">
+        <h3>Registration Successful!</h3>
+        <p>Your form has been submitted successfully.</p>
+    </div>
+    <a href="registration1.php" class="btn btn-primary">Go Back</a>
+</div>
+<?php 
+include("footer.php");
+?>
