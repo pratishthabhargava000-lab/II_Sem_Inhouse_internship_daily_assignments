5ab0ea7368aec409b5724d4427650c9f7a595886
pratishthabhargava000-lab <pratishthabhargava000@gmail.com>
2026-07-11 13:26:51 +0530

    Create contact.php

---
+++ Day 12/contact.php
@@ -0,0 +1,13 @@
+<?php
+include("dashboardheader.php");
+?>
+
+<div class="container mt-5">
+    <h1>Contact Us</h1>
+    <p>Email: support@gmail.com</p>
+    <p>Phone: +91 9876543210</p>
+    <p>Location: Jaipur, Rajasthan</p>
+</div>
+<?php
+include("footer.php");
+?>
