db7ef1d12c30cd9aea9f018683dbd94c278d63dd
pratishthabhargava000-lab <pratishthabhargava000@gmail.com>
2026-07-11 13:17:33 +0530

    Create about.php

---
+++ Day 12/about.php
@@ -0,0 +1,13 @@
+<?php
+include("dashboardheader.php");
+?>
+
+<div class="container mt-5">
+    <h1>About Us</h1>
+    <p>This website is developed using HTML, CSS, PHP, and MySQL.
+         It includes Login,Registration and Update Password features.
+    </p>
+</div>
+<?php
+include("footer.php");
+?>
